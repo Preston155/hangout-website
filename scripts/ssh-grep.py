576492a84bd4c908e512cd43731d9e3f92f97ed4
Preston155 <Preston155@users.noreply.github.com>
2026-06-15 21:01:57 -0400

    Auto publish website update - 2026-06-15 21:01:57

--- scripts/ssh-grep.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-c=paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect('172.82.64.171', username='root', password='0625$$Odie$$', timeout=25)
-for f in ['purge.js','server-status.js']:
-    _i,o,_e=c.exec_command(f'grep -n "SlashCommandBuilder\\|setName\\|setDescription\\|prefixName\\|aliases" /root/bots/bot3/src/commands/{f}')
-    print('===',f,'===')
-    print(o.read().decode())
-c.close()
